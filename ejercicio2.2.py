de7c5d89e908cf8ae1a07b65871465b30007a1f3
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2022-09-22 10:15:59 +0200

    Add files via upload

---
+++ ejercicio2.2.py
@@ -0,0 +1,16 @@
+#Escriba un algoritmo que determine la categoría deportiva de un usuario
+#en función de su edad.
+''' - 6 a 7 años: benjamin
+    - 8 a 9 años:  alevín
+    - 10 a 11 años: infantil
+    - 12 años y más: cadete
+'''
+
+edad= 0
+
+print ("Dime tu edad : ")
+
+edad = input()
+
+if (6<edad<7)
+
