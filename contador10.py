788a13dd00eb5633650ff3b07d53a3719bbf2de0
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2022-09-23 10:58:22 +0200

    Add files via upload

---
+++ contador10.py
@@ -0,0 +1,16 @@
+#Realizar un contador hasta 10 usando while
+
+#variable contador
+
+contador=0
+termina = True
+
+while(termina):
+    
+    contador = contador + 1
+    print(contador)
+    if(contador == 10):
+        termina = False
+        
+    
+print("Fin")
---
+++ while.py
@@ -0,0 +1,8 @@
+termina = True
+
+while(termina):
+    mensaje = input("Dime algo: ")
+    if(mensaje == "fin"):
+        termina = False
+    
+print("Finalizado")
