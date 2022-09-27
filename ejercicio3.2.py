c2416c8a472a06cee94cbaf9be0d37b04d1d53e8
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2022-09-27 10:08:35 +0200

    Add files via upload

--- ejercicio3.2.py
+++ ejercicio3.2.py
@@ -2,9 +2,17 @@
 #números enteros múltiplos de tres, hay comprendidos entre la unidad y dicho
 #número.
 
-num = 0
+num = -1
+total_multiplos = 0
+indice = 1
 
+#Pregunto al usuario el número.
 num = int(input ("Dime un número: "))
 
-indice%3==0
+while (indice<=num):
+    if(indice%3==0):
+        total_multiplos += 1 #total_multiplos = total_multiplos + 1
+    indice = indice + 1
+
+print ("Entre la unidad y",num,"hay un total de",total_multiplos,"números multiplos de 3.")
 
