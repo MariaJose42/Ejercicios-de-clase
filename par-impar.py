2289cda62699f915020b4da433f1091304b48e36
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2022-09-29 19:11:57 +0200

    Add files via upload

---
+++ par-impar.py
@@ -0,0 +1,14 @@
+# Dando un número decir si es par o impar.
+
+num = -1
+
+
+#Pregunto al usuario el número.
+num = int(input ("Dime un número: "))
+
+if(num%2==0):
+    print("Es par")
+    
+else:
+    print("Es impar")
+        
